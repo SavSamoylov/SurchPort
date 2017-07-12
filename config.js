8a0073bc9cf243c954636033d42e91785f7e0363
Saveliy <samoylov3@hotmail.com>
2017-07-11 23:37:01 -0400

    none

---
+++ config.js
@@ -0,0 +1,19 @@
+const config = {
+    fs: { //  FOURSQUARE API    
+        clientID: "IHMKAGTH1OPVZB11OJUS3YVATBRZGA4GXJFAAIVLJHSVYIVX",
+        clientSECRET: "WN5TRJG5MXTFC3IXRWFFZ4WVELP13KPFB42DXZVJJ3MRLDTA",
+    },    
+    gm: {
+        key: "AIzaSyDRH-_Jw8Jwf_T6LZt3Y5XJh1KSOsPqO0I",
+    },
+    fBase: {
+        config: {
+                apiKey: "AIzaSyDKSWcHpLVdAqZCM6LA74lPZxxYHPt9BLk",
+                authDomain: "spfavorites-1b505.firebaseapp.com",
+                databaseURL: "https://spfavorites-1b505.firebaseio.com",
+                projectId: "spfavorites-1b505",
+                storageBucket: "",
+                messagingSenderId: "301745670269"
+              },
+    },
+}
